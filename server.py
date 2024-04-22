0a95c96f048bec96fc77a305ebc7e17999c9d31e
Ted <58144503+BUSH222@users.noreply.github.com>
2024-04-22 10:09:32 +0300

    flake8, cleanup

--- server.py
+++ server.py
@@ -1,37 +1,37 @@
 from flask import Flask, request
-import json
 import random
 import string
 
 app = Flask(__name__)
- 
-fundata = {} #{roomid:[poss, move]}
+
+fundata = {}  # {roomid:[poss, move]}
+
 
 @app.route('/sendmove', methods=['POST'])
 def testdata():
     print(request.args.get('roomid'))
     return 'ok'
 
+
 @app.route('/receive', methods=['POST'])
 def receive():
     rid = request.args.get('roomid')
     return fundata[rid]
+
 
 @app.route('/creategame', methods=['GET'])
 def savegame():
     for i in range(6):
         newroom = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
         if newroom not in fundata.keys():
-            fundata[newroom]=[[], 'r']
+            fundata[newroom] = [[], 'r']
             return newroom
     return None
 
 
-
-
 @app.route('/')
 def index():
-  return "<html><body><h1>Welcome. Website is running.</h1></body></html>"
+    return "<html><body><h1>Welcome. Website is running.</h1></body></html>"
 
 
 if __name__ == "__main__":
--- testserver.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-params ={"roomid":"wow234"}
-
-requests.post('http://127.0.0.1:5000/sendmove', params=params)
